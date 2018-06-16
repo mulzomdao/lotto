be55fd0a0ae0f5541d5f554f0d7dfda2ea34f5f2
mulzomdao <mulzomdao@gmail.com>
2018-06-16 01:10:57 +0000

    3333

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,28 @@
 package project;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
+    
+    private Random random = new Random(System.nanoTime());
+    
+    public int[] generate() {
+        int[] result = new int[6];
+        for (int i = 0; i < 6; i++) {
+            result[i] = random.nextInt(45);
+        }
+        
+        return result;
+    }
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+        
+        int[] result = new Main().generate();
+        System.out.println(Arrays.toString(result));
     }
 
 }
